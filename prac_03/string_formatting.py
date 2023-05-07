6f265e80a7f1c370c5ff8fdc87345f76718063bf
chlstndy <128973426+chlstndy@users.noreply.github.com>
2023-05-08 00:09:58 +0800

    Add files via upload

---
+++ prac_03/string_formatting.py
@@ -0,0 +1,10 @@
+# 1.
+year = 1922
+make = "Gibson"
+model = "L-5 CES"
+cost = 16035.4
+print(f"{year} {make} {model} for about ${cost:,.2f}!")
+
+# 2.
+for i in range(0, 151, 50):
+    print(f"{i:>3}")
